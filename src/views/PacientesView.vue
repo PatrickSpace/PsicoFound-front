4568b134b3606547e1e2855854e990631ada6c8b
PatrickSpace <jplosta01@gmail.com>
2024-11-01 15:22:23 -0500

    layout

--- src/views/PacientesView.vue
+++ src/views/PacientesView.vue
@@ -1,6 +1,13 @@
 <template>
+  <v-app app>
+
+  </v-app>
   <v-container>
     <h1>Pacientes</h1>
     <v-divider></v-divider>
   </v-container>
 </template>
+<script>
+import Navbar from "@components/Layout/NavBar"
+
+</script>
