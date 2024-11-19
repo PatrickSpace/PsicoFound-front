b1ea55ebaff66c08b23ddc7fca66c66e500f8b65
PatrickSpace <jplosta01@gmail.com>
2024-11-19 08:58:08 -0500

    encuesta

--- src/components/encuesta/EncuestaTree.vue
+++ src/components/encuesta/EncuestaTree.vue
@@ -2,35 +2,69 @@
   <div>
     <v-stepper v-model="e1" class="bg-transparent elevation-0">
       <template v-slot:default="{ prev, next }">
+
         <v-stepper-header class="elevation-0" color="white">
           <template v-for="paso in pasos" :key="paso.key">
-            <v-stepper-item
-              :title="paso.value"
-              :complete="e1 > paso.n"
-              :step="`Step {{ paso.n }}`"
-              :value="paso.n"
-              color="white"
-            ></v-stepper-item>
-
+            <v-stepper-item :title="paso.value" :complete="e1 > paso.n" :step="`Step {{ paso.n }}`" :value="paso.n"
+              color="white"></v-stepper-item>
             <v-divider v-if="paso.n !== steps" :key="paso.n"></v-divider>
           </template>
         </v-stepper-header>
 
         <v-stepper-window>
-          <v-stepper-window-item
-            v-for="paso in pasos"
-            :key="paso.key"
-            :value="paso.n"
-          >
-            <v-card color="grey-lighten-1" height="200"></v-card>
+
+
+          <v-stepper-window-item :value="1">
+            <v-card class="bg-transparent w-100">
+              <h1 class="text-h5 text-center">¿Que probema deseas resolver?</h1>
+              <v-container class="mx-auto w-50 mt-5">
+                <v-combobox v-model="especialidad" chips multiple
+                  :items="['Abuso de sustancias', 'Ansiedad', 'Depresión', 'Trauma infatil', 'Ansiedad social']"></v-combobox>
+              </v-container>
+            </v-card>
           </v-stepper-window-item>
+
+          <v-stepper-window-item :value="2">
+            <v-card class="bg-transparent w-100">
+              <h1 class="text-h5 text-center">¿Como te gustaria llevar tu terapia?</h1>
+              <v-container class="mx-auto w-50 mt-5">
+                <v-combobox v-model="enfoque"
+                  :items="['Humanista', 'Cognitivo-Conductual', 'Psicoanalisis']"></v-combobox>
+              </v-container>
+            </v-card>
+          </v-stepper-window-item>
+
+          <v-stepper-window-item :value="3">
+            <v-card class="bg-transparent w-100">
+              <h1 class="text-h5 text-center">¿Con que genero de terapeuta te sientes más comodo?</h1>
+              <v-container class="mx-auto w-50 mt-5">
+                <v-combobox v-model="genero" :items="['Hombre', 'Mujer', 'Me es indiferente']"></v-combobox>
+              </v-container>
+            </v-card>
+          </v-stepper-window-item>
+
+          <v-stepper-window-item :value="4">
+            <v-card class="bg-transparent w-100">
+              <h1 class="text-h5 text-center">¿Que modalidad te gustaria seguir?</h1>
+              <v-container class="mx-auto w-50 mt-5">
+                <v-combobox v-model="modalidad" :items="['Online', 'Presencial', 'Hiibrido']"></v-combobox>
+              </v-container>
+            </v-card>
+          </v-stepper-window-item>
+
+          <v-stepper-window-item :value="5">
+            <v-card class="bg-transparent w-100">
+              <h1 class="text-h5 text-center">¿Que edad te gusrtaria que tenga tu terapeuta</h1>
+              <v-container class="mx-auto w-50 mt-5 text-center">
+                <v-combobox v-model="modalidad" :items="['18-25', '25-35', '35-45', '+ 45']"></v-combobox>
+                <v-btn class="mx-auto text-center" variant="tonal" size="large"> Buscar terapeuta</v-btn>
+              </v-container>
+            </v-card>
+          </v-stepper-window-item>
+
         </v-stepper-window>
 
-        <v-stepper-actions
-          :disabled="disabled"
-          @click:next="next"
-          @click:prev="prev"
-        ></v-stepper-actions>
+        <v-stepper-actions :disabled="disabled" @click:next="next" @click:prev="prev"></v-stepper-actions>
       </template>
     </v-stepper>
   </div>
@@ -40,10 +74,18 @@ export default {
   data() {
     return {
       e1: 1,
-      steps: 2,
+      steps: 5,
+      especialidad: "",
+      enfoque: "",
+      genero: "",
+      modalidad: "",
+      edad: "",
       pasos: [
-        { n: 1, key: 1, value: "Paso 1" },
-        { n: 2, key: 2, value: "Paso 2" },
+        { n: 1, key: 1, value: "Especialidad", },
+        { n: 2, key: 2, value: "Enfoque terapeutico", },
+        { n: 3, key: 3, value: "Genero del terapeuta", },
+        { n: 4, key: 4, value: "Modalidad", },
+        { n: 5, key: 5, value: "Edad", }
       ],
     };
   },
@@ -53,8 +95,8 @@ export default {
       return this.e1 === 1
         ? "prev"
         : this.e1 === this.steps
-        ? "next"
-        : undefined;
+          ? "next"
+          : undefined;
     },
   },
 };
