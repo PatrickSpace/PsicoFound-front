88bc9c4d1445b186bb66469c2e3abcc84ffbfb89
PatrickSpace <jplosta01@gmail.com>
2025-08-09 23:38:38 -0500

    chore(core): reemplazar store anterior por configuración de Pinia en main.js

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import App from "./App.vue";
 import router from "./router";
 import apiClient from "./plugins/axios";
 import vuetify from "./plugins/vuetify";
-import store from "./store/store";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
 app.use(router);
 app.use(vuetify);
+app.use(createPinia());
 app.mount("#app");
-app.use(store);
 //axios configuration
 app.config.globalProperties.$axios = apiClient;
